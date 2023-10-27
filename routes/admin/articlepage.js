eb8d133c9949dc493ff29df816db0c9152466271
jason <sxrele@outlook.com>
2023-10-27 19:09:54 +0800

    实现文章列表

--- routes/admin/articlepage.js
+++ routes/admin/articlepage.js
@@ -1,5 +1,14 @@
-module.exports=function (req, res) {
-  req.app.locals.selectmark='article';
+require("../../model/connect")
+const {Aricle} = require("../../model/article");
+const {User}=require('../../model/user')
 
-    res.render("./admin/article", )
+
+module.exports=async function (req, res) {
+  req.app.locals.selectmark='article';
+  let article=await Aricle.find()
+  let author=await User.findById(article._id)
+  console.log(article);
+    res.render("./admin/article", {
+      articledata:article
+    })
   }
